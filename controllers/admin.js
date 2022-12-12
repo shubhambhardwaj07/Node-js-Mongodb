cc7c44dd125ebac3b7f69789b7519e2e107559ac
Shubham Bhardwaj <shubham998845@gmail.com>
2022-12-12 21:57:41 +0530

    file delete if deleted from server and invoice completed

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const fileHelper = require("../util/file");
 const { validationResult } = require("express-validator/check");
 
 const Product = require("../models/product");
@@ -151,6 +152,7 @@ exports.postEditProduct = (req, res, next) => {
       product.price = updatedPrice;
       product.description = updatedDesc;
       if (image) {
+        fileHelper.deleteFile(product.imageUrl);
         product.imageUrl = image.path;
       }
 
@@ -179,7 +181,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteOne({ _id: prodId, userId: req.user._id })
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found"));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
     .then(() => {
       console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
